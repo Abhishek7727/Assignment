145f5dc9d38176dcbb7a95cdaeea611f437b03e1
Abhishek Namdev <vvnamdev@gmail.com>
2024-03-24 14:44:39 +0000

    6.3

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,31 +1,28 @@
- sap.ui.define([
-                    "sap/ui/core/mvc/Controller",
-                    "sap/m/MessageToast",
-                    'sap/ui/model/json/JSONModel'
-                ],
-                    /**
-                     * @param {typeof sap.ui.core.mvc.Controller} Controller
-                     */
-                    function (Controller,MessageToast,JSONModel) {
-                        "use strict";
-                
-                        return Controller.extend("com.project1.controller.View", {
-                            onInit: function () {
-                                this.oModel = new JSONModel({
-                                    name: "",
-                                    email: "",
-                                    date: "",
-                                    domains:[
-                                        { key: "1", text: "Frontend" },
-                                        { key: "2", text: "Backend" },
-                                    ],
-                                    selectedDomain: ""
-                                });
-                                this.getView().setModel(this.oModel);
-                            },
-                            onSubmit: function() {
-                                MessageToast.show("Survey submitted successfully!");
-                                this.getView().byId("thankYouText").setVisible(true);
-                            }
+sap.ui.define([
+                            "sap/ui/core/mvc/Controller",
+                            "sap/m/MessageBox",
+                            "sap/ui/model/json/JSONModel"
+                        ], function (Controller, MessageBox, JSONModel) {
+                            "use strict";
+                        
+                            return Controller.extend("com.project1.controller.View", {
+                        
+                                onInit: function () {
+                                    var imgData = {
+                                        "images": [
+                                          { "url": "https://www.hdcarwallpapers.com/walls/toyota_hilux_gr_sport_ii_double_cab_2023_4k-HD.jpg" },
+                                          { "url": "https://www.hdcarwallpapers.com/walls/toyota_gr_supra_gt4_evo_2023_5k-HD.jpg" },
+                                          { "url": "https://www.hdcarwallpapers.com/walls/toyota_gr_supra_2_0_fuji_speedway_edition_2021_4k_3-HD.jpg" },
+                                          { "url": "https://www.hdcarwallpapers.com/walls/toyota_crown_sedan_prototype_2022_4k_8k_2-HD.jpg" }
+                                        ]
+                                      };
+                                      
+                                      var oImgModel = new sap.ui.model.json.JSONModel();
+                                      oImgModel.setData(imgData);
+                                      
+                                      this.getView().setModel(oImgModel, "img");
+                                      console.log(oImgModel);
+                                }
+                        
+                            });
                         });
-                    })
